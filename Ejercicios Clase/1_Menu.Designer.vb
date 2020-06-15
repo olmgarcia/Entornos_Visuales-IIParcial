e15d8896ab76614b9475d38e7205b1bdb3479887
cliente <cliente@DESKTOP-HD05D0Q>
2020-06-14 22:49:56 -0600

    II Periodo PEV

---
+++ Ejercicios Clase/1_Menu.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class _1_Menu
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(_1_Menu))
+        Me.PanelBarraTitulo = New System.Windows.Forms.Panel()
+        Me.TimerMostrarMenu = New System.Windows.Forms.Timer(Me.components)
+        Me.TimerOcultarMenu = New System.Windows.Forms.Timer(Me.components)
+        Me.BtnMenu = New System.Windows.Forms.Button()
+        Me.PanelMenu = New System.Windows.Forms.Panel()
+        Me.BtnCerrar = New System.Windows.Forms.Button()
+        Me.BtnMaximizar = New System.Windows.Forms.Button()
+        Me.BtnMinimizar = New System.Windows.Forms.Button()
+        Me.BtnRestaurar = New System.Windows.Forms.Button()
+        Me.PanelBarraTitulo.SuspendLayout()
+        Me.PanelMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelBarraTitulo
+        '
+        Me.PanelBarraTitulo.BackColor = System.Drawing.Color.Blue
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnRestaurar)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnMinimizar)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnMaximizar)
+        Me.PanelBarraTitulo.Controls.Add(Me.BtnCerrar)
+        Me.PanelBarraTitulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelBarraTitulo.Location = New System.Drawing.Point(0, 0)
+        Me.PanelBarraTitulo.Name = "PanelBarraTitulo"
+        Me.PanelBarraTitulo.Size = New System.Drawing.Size(1150, 40)
+        Me.PanelBarraTitulo.TabIndex = 0
+        '
+        'TimerMostrarMenu
+        '
+        '
+        'TimerOcultarMenu
+        '
+        Me.TimerOcultarMenu.Interval = 15
+        '
+        'BtnMenu
+        '
+        Me.BtnMenu.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMenu.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMenu.Location = New System.Drawing.Point(171, 20)
+        Me.BtnMenu.Name = "BtnMenu"
+        Me.BtnMenu.Size = New System.Drawing.Size(32, 32)
+        Me.BtnMenu.TabIndex = 0
+        Me.BtnMenu.UseVisualStyleBackColor = True
+        '
+        'PanelMenu
+        '
+        Me.PanelMenu.BackColor = System.Drawing.Color.Gray
+        Me.PanelMenu.Controls.Add(Me.BtnMenu)
+        Me.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PanelMenu.Location = New System.Drawing.Point(0, 40)
+        Me.PanelMenu.Name = "PanelMenu"
+        Me.PanelMenu.Size = New System.Drawing.Size(220, 560)
+        Me.PanelMenu.TabIndex = 1
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.FlatAppearance.BorderSize = 0
+        Me.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnCerrar.Image = CType(resources.GetObject("BtnCerrar.Image"), System.Drawing.Image)
+        Me.BtnCerrar.Location = New System.Drawing.Point(1095, 6)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnCerrar.TabIndex = 2
+        Me.BtnCerrar.UseVisualStyleBackColor = True
+        '
+        'BtnMaximizar
+        '
+        Me.BtnMaximizar.FlatAppearance.BorderSize = 0
+        Me.BtnMaximizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnMaximizar.Image = CType(resources.GetObject("BtnMaximizar.Image"), System.Drawing.Image)
+        Me.BtnMaximizar.Location = New System.Drawing.Point(1064, 3)
+        Me.BtnMaximizar.Name = "BtnMaximizar"
+        Me.BtnMaximizar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnMaximizar.TabIndex = 3
+        Me.BtnMaximizar.UseVisualStyleBackColor = True
+        '
+        'BtnMinimizar
+        '
+        Me.BtnMinimizar.FlatAppearance.BorderSize = 0
+        Me.BtnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnMinimizar.Image = CType(resources.GetObject("BtnMinimizar.Image"), System.Drawing.Image)
+        Me.BtnMinimizar.Location = New System.Drawing.Point(1033, 12)
+        Me.BtnMinimizar.Name = "BtnMinimizar"
+        Me.BtnMinimizar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnMinimizar.TabIndex = 4
+        Me.BtnMinimizar.UseVisualStyleBackColor = True
+        '
+        'BtnRestaurar
+        '
+        Me.BtnRestaurar.FlatAppearance.BorderSize = 0
+        Me.BtnRestaurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRestaurar.Image = CType(resources.GetObject("BtnRestaurar.Image"), System.Drawing.Image)
+        Me.BtnRestaurar.Location = New System.Drawing.Point(1064, 6)
+        Me.BtnRestaurar.Name = "BtnRestaurar"
+        Me.BtnRestaurar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnRestaurar.TabIndex = 5
+        Me.BtnRestaurar.UseVisualStyleBackColor = True
+        '
+        '_1_Menu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1150, 600)
+        Me.Controls.Add(Me.PanelMenu)
+        Me.Controls.Add(Me.PanelBarraTitulo)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "_1_Menu"
+        Me.Text = "_1_Menu"
+        Me.PanelBarraTitulo.ResumeLayout(False)
+        Me.PanelMenu.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelBarraTitulo As Panel
+    Friend WithEvents TimerMostrarMenu As Timer
+    Friend WithEvents TimerOcultarMenu As Timer
+    Friend WithEvents BtnMenu As Button
+    Friend WithEvents PanelMenu As Panel
+    Friend WithEvents BtnCerrar As Button
+    Friend WithEvents BtnMaximizar As Button
+    Friend WithEvents BtnRestaurar As Button
+    Friend WithEvents BtnMinimizar As Button
+End Class
---
+++ Ejercicios Clase/1_Menu.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Runtime.InteropServices
+Public Class _1_Menu
+
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(ByVal hWnd As System.IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer)
+    End Sub
+    Private Sub PanelContenedor_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub PanelBarraTitulo_Paint(sender As Object, e As PaintEventArgs) Handles PanelBarraTitulo.Paint
+
+    End Sub
+
+    Private Sub PanelBarraTitulo_MouseMove(sender As Object, e As MouseEventArgs) Handles PanelBarraTitulo.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+    Private Sub TimerMostrarMenu_Tick(sender As Object, e As EventArgs) Handles TimerMostrarMenu.Tick
+        If PanelMenu.Width >= 220 Then
+            Me.TimerMostrarMenu.Enabled = False
+        Else
+            Me.PanelMenu.Width = PanelMenu.Width + 5
+        End If
+    End Sub
+
+    Private Sub TimerOcultarMenu_Tick(sender As Object, e As EventArgs) Handles TimerOcultarMenu.Tick
+        If PanelMenu.Width <= 60 Then
+            Me.TimerOcultarMenu.Enabled = False
+        Else
+            Me.PanelMenu.Width = PanelMenu.Width - 5
+        End If
+    End Sub
+
+    Private Sub BtnMenu_Click(sender As Object, e As EventArgs) Handles BtnMenu.Click
+        If PanelMenu.Width = 220 Then
+            TimerOcultarMenu.Enabled = True
+        ElseIf PanelMenu.Width = 60 Then
+            TimerMostrarMenu.Enabled = True
+        End If
+    End Sub
+
+    Private Sub BtnCerrar_Click_1(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub BtnMaximizar_Click(sender As Object, e As EventArgs) Handles BtnMaximizar.Click
+        BtnMaximizar.Visible = False
+        BtnRestaurar.Visible = True
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub BtnMinimizar_Click(sender As Object, e As EventArgs) Handles BtnMinimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub BtnRestaurar_Click(sender As Object, e As EventArgs) Handles BtnRestaurar.Click
+        Me.WindowState = FormWindowState.Normal
+        BtnRestaurar.Visible = False
+        BtnMaximizar.Visible = True
+    End Sub
+End Class
